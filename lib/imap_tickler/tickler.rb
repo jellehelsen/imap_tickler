8816be0fe99ad58c31ebf53922b25d131017c7bb
Jelle Helsen <jelle.helsen@hcode.be>
2011-02-09 10:08:18 +0100

    adding daily mailbox list

--- lib/imap_tickler/tickler.rb
+++ lib/imap_tickler/tickler.rb
@@ -2,6 +2,11 @@ require "date"
 module ImapTickler
   class Tickler
     def initialize(config)
+      @day_list = ["Week 1/1", "Week 1/2", "Week 1/3", "Week 1/4", "Week 1/5", "Week 1/6", "Week 1/7", "Week 2/8", "Week 2/9",
+        "Week 2/10", "Week 2/11", "Week 2/12", "Week 2/13", "Week 2/14", "Week 3/15", "Week 3/16", "Week 3/17", "Week 3/18",
+        "Week 3/19", "Week 3/20", "Week 3/21", "Week 4/22", "Week 4/23", "Week 4/24", "Week 4/25", "Week 4/26", "Week 4/27", 
+        "Week 4/28", "Week 5/29", "Week 5/30", "Week 5/31"]
+
       @month_list = ["Month/01 Jan", "Month/02 Feb", "Month/03 Mar", "Month/04 Apr", "Month/05 May", "Month/06 Jun", 
         "Month/07 Jul", "Month/08 Aug", "Month/09 Sep", "Month/10 Oct", "Month/11 Nov", "Month/12 Dec"]
       
@@ -9,6 +14,10 @@ module ImapTickler
   
     def this_months_mailbox
       @month_list[Date.today.month - 1]
+    end
+  
+    def todays_mailbox
+      @day_list[Date.today.day - 1]
     end
   end
 end
--- spec/lib/ticker_spec.rb
+++ spec/lib/ticker_spec.rb
@@ -3,9 +3,23 @@ describe ImapTickler::Tickler do
   before do
     @tickler = ImapTickler::Tickler.new({})
   end
-  it "should get the correct month" do
+  it "should get the correct mailbox for this month" do
     Delorean.time_travel_to "5 Jan 2011"
     @tickler.this_months_mailbox.should == "Month/01 Jan"
+    Delorean.time_travel_to "5 Feb 2011"
+    @tickler.this_months_mailbox.should == "Month/02 Feb"
+    Delorean.time_travel_to "5 Dec 2011"
+    @tickler.this_months_mailbox.should == "Month/12 Dec"
+    Delorean.back_to_the_present
+  end
+
+  it "should get the correct mailbox for today" do
+    Delorean.time_travel_to "5 Jan 2011"
+    @tickler.todays_mailbox.should == "Week 1/5"
+    Delorean.time_travel_to "15 Feb 2011"
+    @tickler.todays_mailbox.should == "Week 3/15"
+    Delorean.time_travel_to "25 Dec 2011"
+    @tickler.todays_mailbox.should == "Week 4/25"
     Delorean.back_to_the_present
   end
 end
