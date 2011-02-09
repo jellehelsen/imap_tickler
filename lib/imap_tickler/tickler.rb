fe8dd28851c24266d2e747e699e2d0d0f10edb76
Jelle Helsen <jelle.helsen@hcode.be>
2011-02-09 10:01:31 +0100

    adding month list

--- lib/imap_tickler/tickler.rb
+++ lib/imap_tickler/tickler.rb
@@ -1,9 +1,14 @@
+require "date"
 module ImapTickler
   class Tickler
     def initialize(config)
+      @month_list = ["Month/01 Jan", "Month/02 Feb", "Month/03 Mar", "Month/04 Apr", "Month/05 May", "Month/06 Jun", 
+        "Month/07 Jul", "Month/08 Aug", "Month/09 Sep", "Month/10 Oct", "Month/11 Nov", "Month/12 Dec"]
       
     end
   
-  
+    def this_months_mailbox
+      @month_list[Date.today.month - 1]
+    end
   end
 end
--- spec/lib/ticker_spec.rb
+++ spec/lib/ticker_spec.rb
@@ -1,6 +1,11 @@
 require "spec_helper"
 describe ImapTickler::Tickler do 
+  before do
+    @tickler = ImapTickler::Tickler.new({})
+  end
   it "should get the correct month" do
-    
+    Delorean.time_travel_to "5 Jan 2011"
+    @tickler.this_months_mailbox.should == "Month/01 Jan"
+    Delorean.back_to_the_present
   end
 end
